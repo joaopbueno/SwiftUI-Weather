b1b0c98acffbb33d1fdf6051388d11984305d05b
joaopbueno <mrjoao16@gmail.com>
2024-11-22 15:59:18 -0300

    InterfaceFinalizada

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -19,6 +19,39 @@ struct ContentView: View {
                     .font(.system(size:32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .bold()
+                    .padding()
+                VStack(spacing: 8){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("26°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                    
+                        
+                }
+                .padding(.bottom, 40)
+                HStack(spacing: 30) {
+                    WeatherDayView(diaDaSemana: "TUE", imageName: "sun.max.fill", temperatura: 26)
+                    WeatherDayView(diaDaSemana: "WED", imageName: "cloud.sun.fill", temperatura: 20)
+                    WeatherDayView(diaDaSemana: "THU", imageName: "wind.snow", temperatura: 21)
+                    WeatherDayView(diaDaSemana: "FRI", imageName: "cloud.snow.fill", temperatura: -5)
+                    WeatherDayView(diaDaSemana: "SAT", imageName: "cloud.rain.fill", temperatura: 16)
+            }
+            Spacer()
+                Button {
+                    print("tapped")
+                } label: {
+                    Text("Change Day Time")
+                        .frame(width: 280, height: 50)
+                        .background(Color.white)
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                        .cornerRadius(15)
+                }
+            Spacer()
             }
         }
     }
@@ -26,4 +59,28 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct WeatherDayView: View {
+    
+    var diaDaSemana : String
+    var imageName : String
+    var temperatura : Int
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Text(diaDaSemana)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperatura)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+                .bold()
+        }
+    }
 }
